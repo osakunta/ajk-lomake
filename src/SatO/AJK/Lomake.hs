d61e0e453cf5d67acde214f52ba2b72d0afe9306
Oleg Grenrus <oleg.grenrus@iki.fi>
2016-05-17 20:43:01 +0300

    WIP: sisanen haku

--- src/SatO/AJK/Lomake.hs
+++ src/SatO/AJK/Lomake.hs
@@ -4,9 +4,8 @@
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE RankNTypes          #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TemplateHaskell     #-}
-{-# LANGUAGE TypeFamilies        #-}
 {-# LANGUAGE TypeOperators       #-}
+{-# LANGUAGE TemplateHaskell     #-}
 module SatO.AJK.Lomake (
     defaultMain,
     Person(..),
@@ -29,7 +28,6 @@ import Data.Maybe                (fromMaybe)
 import Data.Semigroup            ((<>))
 import Data.String               (fromString)
 import Data.Text                 (Text)
-import Generics.SOP.TH           (deriveGeneric)
 import Lucid
 import Network.HTTP.Types.Status (status500)
 import Network.Mail.Mime
@@ -46,179 +44,8 @@ import qualified Network.Wai.Handler.Warp as Warp
 
 import Lomake
 
-data Siv = Naimaton | Aviossa
-    deriving (Eq, Show, Enum, Bounded)
-
-data Jasen = Varsinainen | Ulko
-    deriving (Eq, Show, Enum, Bounded)
-
-data Parents = BothAlive | OneAlive | Dead | YH
-    deriving (Eq, Show, Enum, Bounded)
-
-newtype LongText = LongText Text
-
-data Person = Person
-    { personFirstName  :: D "Etunimet"                        'Required Text
-    , personCallName   :: D "Puhuttelunimi"                   'Optional Text
-    , personLastName   :: D "Sukunimet"                       'Required Text
-    , personBirthday   :: D "Syntymäaika"                     'Required Text
-    , personBirthplace :: D "Syntymäpaikka"                   'Required Text
-    , personAddress    :: D "Postiosoite"                     'Required Text
-    , personSivilis    :: D "Siviilisääty"                    'Required Siv
-    , personEmail      :: D "Sähköpostiosoite"                'Required Text
-    , personChildren   :: D "Alaikäiset lapset (syntymäajat)" 'Optional Text
-    , personFacebook   :: D "Kotisivu tai sivu facebookissa"  'Optional Text
-    }
-
-data Studies = Studies
-    { studiesYO        :: D "Ylioppilastutkinnon suoritusvuosi" 'Required Text
-    , studiesLukio     :: D "Lukio"                             'Required Text
-    , studiesYliopisto :: D "Yliopisto"                         'Required Text
-    , studiesTDK       :: D "Tiedekunta ja koulutusohjelma"     'Required Text
-    , studiesTime      :: D "Opintojen alku-/päätösaika"        'Required Text
-    , studiesUsed      :: D "Suoritettava tutkinto"             'Optional Text
-    , studiesDone      :: D "Suoritetut korkeakoulututkinnot"   'Optional Text
-    }
-
-data Talous = Talous
-    { talousTulo        :: D "Verotettava tulo vuonna 2014"                                     'Required Text
-    , talousVar         :: D "Verotettava varallisuus vuonna 2014"                              'Required Text
-    , talousBrutto      :: D "Bruttotulot vuodelta 2014"                                        'Required Text
-    , talousArvio       :: D "Arvio bruttotuloista vuonna 2015"                                 'Required Text
-    , talousLaatu       :: D "Tulon laatu 2015 (ansio-, eläke-, pääoma-, vuokra- tai muu tulo)" 'Required Text
-    , talousEmployer    :: D "Työnantaja vuonna 2015"                                           'Optional Text
-    , talousPuolisoTulo :: D "Arvio puolison bruttotuloista vuonna 2015"                        'Optional Text
-    , talousPuolisoJob  :: D "Puolison ammatti"                                                 'Optional Text
-    , talousApuraha     :: D "Apurahat vuosina 2012-2015 (myöntäjä ja määrä)"                   'Optional Text
-    }
-
-data Family = Family
-    { familyParents          :: D "Vanhemmista"                                                     'Required Parents
-    , familyAlaSisar         :: D "Alaikäisten sisarusten lukumäärä"                                'Optional Text
-    , familyDadJob           :: D "Isän ammatti"                                                    'Optional Text
-    , familyMomJob           :: D "Äidin ammatti"                                                   'Optional Text
-    , familyStudySisar       :: D "Opiskelevien täysi-ikäisten sisarusten lukumäärä"                'Optional Text
-    , familySisarNames       :: D "Sisarusten etunimet"                                             'Optional Text
-    , familyHkiAppartment    :: D "Omistatko tai omistavatko vanhempasi asunnon Helsingin seudulla" 'Required Bool
-    , familuHkiAppartmentUse :: D "Onko asunto perheen omassa käytössä"                             'Optional Bool
-    }
-
-data Satakunta = Satakunta
-    { satakuntaSiteet  :: D "Siteeni Satakuntaan (omin sanoin)"                                       'Required LongText
-    , satakuntaFrom    :: D "Hakija on syntynyt, opiskellut tai asunut Satakunnassa"                  'Required Bool
-    , satakuntaParents :: D "Hakijan vanhemmista jompikumpi on kuulunut Satakuntalaiseen osakuntaan"  'Required Bool
-    , satakuntaTiedots :: D "Hakijan tietoja saa käyttää Satakuntalaisen Osakunnan tiedotukseen"      'Required Bool
-    }
-
-data Osakunta = Osakunta
-    { osakuntaKirja :: D "Osakuntaan kirjoittautumisen lukukausi ja vuosi" 'Required Text
-    , osakuntaJasen :: D "Hakija on Satakuntalaisen osakunnan"             'Optional Jasen
-    }
-
-data OtherInfo = OtherInfo
-    { otherInfoWhen :: D "Milloin voin vastaanottaa asunnon" 'Required Text
-    }
-
-data Overall = Overall
-    { overallHobbies   :: D "Viisi tärkeintä harrastustani"                                          'Optional LongText
-    , overallLuottamus :: D "Luottamustoimeni ja muu toimintani järjestöissä ja/tai yhdistyksissä"   'Optional LongText
-    , overallBio       :: D "Luonnehdi halutessasi itseäsi (max 30 sanaa)"                           'Optional LongText
-    , overallWhyYO     :: D "Miksi pyrit korkeakouluun opiskelemaan/miksi opiskelet korkeakoulussa"  'Optional LongText
-    , overallWhyYou    :: D "Miksi juuri sinun pitäisi päästä asumaan Satalinnan Säätiön asuntoihin" 'Optional LongText
-    }
-
-data AJK = AJK
-    { ajkPerson    :: D "Henkilötiedot"                   'Required Person
-    , ajkStudies   :: D "Opinnot"                         'Required Studies
-    , ajkTalous    :: D "Taloudellinen asema"             'Required Talous
-    , ajkFamily    :: D "Perhesuhteet"                    'Required Family
-    , ajkSatakunta :: D "Yhteydet Satakuntaan"            'Required Satakunta
-    , ajkOsakunta  :: D "Ei koske opiskelunsa aloittavia" 'Required Osakunta
-    , ajkOtherInfo :: D "Muita tietoja"                   'Required OtherInfo
-    , ajkOverall   :: D "Yleistietoja"                    'Required Overall
-    }
-
-deriveGeneric ''Person
-deriveGeneric ''Studies
-deriveGeneric ''Talous
-deriveGeneric ''Family
-deriveGeneric ''Satakunta
-deriveGeneric ''Osakunta
-deriveGeneric ''OtherInfo
-deriveGeneric ''Overall
-
-deriveGeneric ''AJK
-
--------------------------------------------------------------------------------
--- Sections
--------------------------------------------------------------------------------
-
-instance LomakeSection Person
-instance LomakeSection Studies
-instance LomakeSection Talous
-instance LomakeSection Family
-instance LomakeSection Satakunta
-instance LomakeSection Osakunta
-instance LomakeSection OtherInfo
-instance LomakeSection Overall
-
-instance LomakeForm AJK
-
--------------------------------------------------------------------------------
--- Siv
--------------------------------------------------------------------------------
-
-instance HumanShow Siv where
-    humanShow = show
-
-instance LomakeEnum Siv
-
-instance LomakeField Siv where
-    lomakeFieldView   = enumLomakeFieldView
-    lomakeFieldValidate  = enumLomakeFieldValidate
-    lomakeFieldPretty = text . humanShow
-
--------------------------------------------------------------------------------
--- Parents
--------------------------------------------------------------------------------
-
-instance HumanShow Parents where
-    humanShow BothAlive = "Molemmat elävät"
-    humanShow OneAlive  = "Vain toinen elää"
-    humanShow Dead      = "Molemmat kuolleet"
-    humanShow YH        = "Yh. äiti/isä"
-
-instance LomakeEnum Parents
-
-instance LomakeField Parents where
-    lomakeFieldView   = enumLomakeFieldView
-    lomakeFieldValidate  = enumLomakeFieldValidate
-    lomakeFieldPretty = text . humanShow
-
--------------------------------------------------------------------------------
--- Jasen
--------------------------------------------------------------------------------
-
-instance HumanShow Jasen where
-    humanShow Varsinainen  = "Varsinainen jäsen"
-    humanShow Ulko         = "Ulkojäsen"
-
-instance LomakeEnum Jasen
-
-instance LomakeField Jasen where
-    lomakeFieldView = enumLomakeFieldView
-    lomakeFieldValidate = enumLomakeFieldValidate
-    lomakeFieldPretty = text . humanShow
-
--------------------------------------------------------------------------------
--- LongText
--------------------------------------------------------------------------------
-
-instance LomakeField LongText where
-    lomakeFieldView _ env name =
-       textarea_ [name_ name] $ toHtml $ submittedTextValue env name
-    lomakeFieldPretty (LongText t) = "\n" <> lomakeFieldPretty t <> "\n"
-    lomakeFieldValidate _proxyA proxyReq = ofmap proxyReq LongText . lomakeText proxyReq
+import SatO.AJK.Lomake.Asuntohaku
+import SatO.AJK.Lomake.Sisanen
 
 -------------------------------------------------------------------------------
 -- API
@@ -232,15 +59,22 @@ data IndexPage = IndexPage
     }
 newtype ConfirmPage = ConfirmPage Bool -- Error
 
+data SisanenPage = SisanenPage
+    { _sisPageActionUrl :: !ActionUrl
+    , _sisPageAjk       :: !(LomakeResult Sisanen)
+    }
+
 data Ctx = Ctx
     { _ctxActionUrl :: !ActionUrl
     , _ctxToAddress :: !Address
     }
 
 type AJKLomakeAPI =
-    Get '[HTML] IndexPage
+    "ajk-lomake" :> Get '[HTML] IndexPage
     :<|> "ajk-lomake" :> ReqBody '[FormUrlEncoded] (LomakeResult AJK) :> Post '[HTML] IndexPage
     :<|> "ajk-lomake" :> "send" :> ReqBody '[FormUrlEncoded] (LomakeResult AJK) :> Post '[HTML] ConfirmPage
+    :<|> "sisanen-haku" :> Get '[HTML] SisanenPage
+    :<|> "sisanen-haku" :> ReqBody '[FormUrlEncoded] (LomakeResult Sisanen) :> Post '[HTML] SisanenPage
 
 instance ToHtml IndexPage where
     toHtmlRaw _ = pure ()
@@ -251,6 +85,28 @@ instance ToHtml IndexPage where
                     div_ [class_ "row"] $ div_ [class_ "large-12 columns"] $ do
                         h1_ $ "Hakulomake Satalinnan Säätion vuokraamiin huoneistoihin"
                     lomakeView (Proxy :: Proxy AJK) env
+                    div_ [class_ "row"] $ div_ [class_ "large-12 columns"] $ do
+                        input_ [class_ "medium success button", type_ "submit", value_ "Esikatsele"]
+            Just ajk -> do
+                div_ [class_ "row"] $ div_ [class_ "large-12 columns"] $ do
+                    h2_ $ "Tarkista tietosi vielä kerran:"
+                div_ [class_ "row"] $ div_ [class_ "large-12 columns"] $ do
+                    pre_ $ toHtml $ render $ lomakePretty ajk
+                hr_ []
+                form_ [action_ $ actionUrl <> "send", method_ "POST"] $ do
+                    hiddenForm env
+                    div_ [class_ "row"] $ div_ [class_ "large-12 columns"] $ do
+                        input_ [class_ "medium success button", type_ "submit", value_ "Lähetä"]
+
+instance ToHtml SisanenPage where
+    toHtmlRaw _ = pure ()
+    toHtml (SisanenPage actionUrl (LomakeResult env v)) = page_ "Hakulomake Satalinnan Säätion vuokraamiin huoneistoihin" $ do
+        case v of
+            Nothing -> do
+                form_ [action_ $ actionUrl <> "sisanen-haku", method_ "POST"] $ do
+                    div_ [class_ "row"] $ div_ [class_ "large-12 columns"] $ do
+                        h1_ $ "Hakulomake Satalinnan Säätion vuokraamiin huoneistoihin"
+                    lomakeView (Proxy :: Proxy Sisanen) env
                     div_ [class_ "row"] $ div_ [class_ "large-12 columns"] $ do
                         input_ [class_ "medium success button", type_ "submit", value_ "Esikatsele"]
             Just ajk -> do
@@ -277,6 +133,9 @@ ajkLomakeApi = Proxy
 
 firstPost :: MonadIO m => Ctx -> LomakeResult AJK -> m IndexPage
 firstPost (Ctx actionUrl _) = return . IndexPage actionUrl
+
+firstPostSisanen :: MonadIO m => Ctx -> LomakeResult Sisanen -> m SisanenPage
+firstPostSisanen (Ctx actionUrl _) = return . SisanenPage actionUrl
 
 secondPost :: MonadIO m => Ctx -> LomakeResult AJK -> m ConfirmPage
 secondPost _         (LomakeResult _ Nothing) = pure $ ConfirmPage False
@@ -330,6 +189,8 @@ server :: Ctx -> Server AJKLomakeAPI
 server ctx@(Ctx actionUrl _) = pure (IndexPage actionUrl (LomakeResult emptyLomakeEnv Nothing))
     :<|> firstPost ctx
     :<|> secondPost ctx
+    :<|> pure (SisanenPage actionUrl (LomakeResult emptyLomakeEnv Nothing))
+    :<|> firstPostSisanen ctx
 
 app :: Ctx -> Application
 app ctx = serve ajkLomakeApi (server ctx)
@@ -343,7 +204,7 @@ defaultMain :: IO ()
 defaultMain = do
     port <- lookupEnvWithDefault 8080 "PORT"
     emailAddr <- fromMaybe "foo@example.com" <$> lookupEnv "LOMAKE_EMAILADDR"
-    actionUrl <- fromMaybe "/action-url/"    <$> lookupEnv "LOMAKE_ACTIONURL"
+    actionUrl <- fromMaybe "/"               <$> lookupEnv "LOMAKE_ACTIONURL"
     let ctx = Ctx (T.pack actionUrl) (Address Nothing $ T.pack emailAddr)
     hPutStrLn stderr "Hello, ajk-lomake-api is alive"
     hPutStrLn stderr "Starting web server"

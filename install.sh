18988075c50790edb48526694c75b835aa4650ef
Oleg Grenrus <oleg.grenrus@iki.fi>
2016-06-28 23:02:16 +0300

    Add sleep after supervisor stop

--- install.sh
+++ install.sh
@@ -7,5 +7,6 @@ ghc --version || $(ghc-select ghc-7.10.3)
 
 stack install
 sudo /etc/init.d/supervisor stop
+sleep 1
 sudo cp ~/.local/bin/ajk-lomake-server /usr/local/bin/
 sudo /etc/init.d/supervisor start
